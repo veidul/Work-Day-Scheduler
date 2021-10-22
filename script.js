7a82ad58ea190c38d1821c0ff5fe0b7ac47f3c8b
ludie lambright <ludiefletcher@aol.com>
2021-10-21 19:17:13 -0700

    finished working with .js the information input now saves and loads when the page is reloaded.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+$(document).ready(function () {
+
 var timeblocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var currentHour = moment().hour()
-console.log(currentHour);
 var date = $('#currentDay')
 var past = $('.past')
 var future = $('.future')
@@ -12,29 +13,39 @@ function pastPresentFuture() {
         console.log(textArea[1]);
         var hour = $(this).attr("data-time")
         if (hour == currentHour) {
-            textArea[1].addClass("present")
+            $(textArea[1]).addClass("present")
         }
         else if (hour < currentHour) {
-            textArea[1].addClass("past")
+            $(textArea[1]).addClass("past")
         }
         else {
-            textArea[1].addClass("future")
+            $(textArea[1]).addClass("future")
         }
     })
-    //return moment().hour() < hour ? "future" : moment().hour() === hour ? "present" : "past"
 }
+$(".saveBtn").on("click",function(event){
+    event.preventDefault();
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
 
-$(document).ready(function () {
+$("#hour9 .description").val(localStorage.getItem("hour9"))
+$("#hour10 .description").val(localStorage.getItem("hour10"))
+$("#hour11 .description").val(localStorage.getItem("hour11"))
+$("#hour12 .description").val(localStorage.getItem("hour12"))
+$("#hour13 .description").val(localStorage.getItem("hour13"))
+$("#hour14 .description").val(localStorage.getItem("hour14"))
+$("#hour15 .description").val(localStorage.getItem("hour15"))
+$("#hour16 .description").val(localStorage.getItem("hour16"))
+$("#hour17 .description").val(localStorage.getItem("hour17"))
 
     date.text(moment().format("dddd MMMM, Do, YYYY"))
 
-    pastPresentFuture();
     var blocks = $('.time-block');
-
-
+    
+    
     // onclick function save button is gonna save to local storage
-    $(blocks).on("click",function(event){
-        event.preventDefault();
-        console.log(event.target);
-    })
+    pastPresentFuture();
 })
