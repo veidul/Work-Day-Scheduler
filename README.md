80c67df874f462e285041a362a087dd2c84bc0c0
ludie lambright <ludiefletcher@aol.com>
2021-10-23 10:08:25 -0700

    final commit updated javascript with comments, added screenshot and live deployed website link to README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Work-Day-Scheduler
+Welcome to the README for my Work-Day-Scheduler web application.
+
+Using this application you will be able to save a schedule for your current 9-5 workday!
+
+In this web application I used the skills I have obtain thus far in my 
+
+coding bootcamp. Using bootstrap and jquery to make my javascript work. I added
+
+the current day time with moment. When the time of the day has passed the time of
+
+the day when you had something scheduled, the text box for that hour will turn red.
+
+If the time in the text box is current it will be green. When you save your text and reload
+
+the browser your text will return as it was saved.
+
+
+
+[work-day-scheduler deployed](https://veidul.github.io/Work-Day-Scheduler/)
+![work-day-scheduler screenshot](./screenshots/work-day-schedule-ss.png)
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+//using jquery for all of my JS
 $(document).ready(function () {
-
+//declared my variables
 var timeblocks = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var currentHour = moment().hour()
 var date = $('#currentDay')
 var past = $('.past')
 var future = $('.future')
 var present = $('.present')
-
+//function to change the class if past present or future
 function pastPresentFuture() {
     $(".container .time-block").each(function () {
         var textArea = $(this).children()
@@ -23,6 +24,7 @@ function pastPresentFuture() {
         }
     })
 }
+//on click event saves value of time and text
 $(".saveBtn").on("click",function(event){
     event.preventDefault();
     console.log(this);
@@ -30,7 +32,7 @@ $(".saveBtn").on("click",function(event){
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
 })
-
+//saves the description to local storage
 $("#hour9 .description").val(localStorage.getItem("hour9"))
 $("#hour10 .description").val(localStorage.getItem("hour10"))
 $("#hour11 .description").val(localStorage.getItem("hour11"))
@@ -41,6 +43,7 @@ $("#hour15 .description").val(localStorage.getItem("hour15"))
 $("#hour16 .description").val(localStorage.getItem("hour16"))
 $("#hour17 .description").val(localStorage.getItem("hour17"))
 
+//adds the date and time to page header
     date.text(moment().format("dddd MMMM, Do, YYYY"))
 
     var blocks = $('.time-block');
